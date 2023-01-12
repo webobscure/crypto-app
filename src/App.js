577654c745b754a26b8132646c4bda4e1a5fc280
webobscure <sparkygino@gmail.com>
2023-01-12 21:20:54 +0300

    routing navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Switch, Route, Link} from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
-import {Navbar} from './components';
+import { Navbar, HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
 
 function App() {
   return (
     <div className="app">
-        <Navbar />
+      <Navbar />
       <div className="main">
-
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
       </div>
       <div className="footer"></div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,9 +20,11 @@ const Navbar = () => {
             </Menu.Item>
             <Menu.Item icon={<FundOutlined />}>
                 <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-            </Menu.Item><Menu.Item icon={<MoneyCollectOutlined />}>
+            </Menu.Item>
+            <Menu.Item icon={<MoneyCollectOutlined />}>
                 <Link to="/exchanges">Exchanges</Link>
-            </Menu.Item><Menu.Item icon={<BulbOutlined />}>
+            </Menu.Item>
+            <Menu.Item icon={<BulbOutlined />}>
                 <Link to="/news">News</Link>
             </Menu.Item>
         </Menu>
