0c4afe17915ec9ac6f1b89a3bc83f79a931578a2
webobscure <obscurium@icloud.com>
2023-01-28 11:22:24 +0300

    change to another api

--- src/services/CoinApi.js
+++ src/services/CoinApi.js
@@ -7,7 +7,7 @@ const options = {
     "headers": {'X-CoinAPI-Key': '817211F9-C239-4AF8-AA46-18E525CA168B'}
 };
 
-export const coinApi = axios.get(options, function (response) {
+export const coinApi = axios.getUri(options, function (response) {
     const chunks = [];
     response.on("data", function (chunk) {
       chunks.push(chunk);
